498f563a84024f91b193abfe4742de9d0f25cc9c
Summit <ssharker21github@gmail.com>
2020-08-25 22:33:57 -0400

    No longer a crash but not working

--- index.js
+++ index.js
@@ -10,10 +10,37 @@ const quoteIcon = "https://cdn.discordapp.com/attachments/449680513683292162/746
 const emptyIcon = "https://cdn.discordapp.com/attachments/449680513683292162/746829996752109678/Untitled.png";
 const sp = "📕 Scarlet Pimpernel by Baroness Orczy";
 const helpDomain = configFile['help-domain'] ? configFile['help-domain'] : undefined;
+const axios = require("axios");
 const randKey = obj => {
     var keys = Object.keys(obj);
     return keys[keys.length * Math.random() << 0];
 }; // gets random key from an object
+const exampleEmbed = (
+	temp,
+	maxTemp,
+	minTemp,
+	pressure,
+	humidity,
+	wind,
+	cloudness,
+	icon,
+	author,
+	profile,
+	cityName,
+	country
+) =>
+	new Discord.RichEmbed()
+		.setColor('#0099ff')
+		.setAuthor(`Hello, ${author}`, profile)
+		.setTitle(`There is ${temp}\u00B0 C in ${cityName}, ${country}`)
+		.addField(`Maximum Temperature:`, `${maxTemp}\u00B0 C`, true)
+		.addField(`Minimum Temperature:`, `${minTemp}\u00B0 C`, true)
+		.addField(`Humidity:`, `${humidity} %`, true)
+		.addField(`Wind Speed:`, `${wind} m/s`, true)
+		.addField(`Pressure:`, `${pressure} hpa`, true)
+		.addField(`Cloudiness:`, `${cloudness}`, true)
+		.setThumbnail(`http://openweathermap.org/img/w/${icon}.png`)
+		.setFooter('Made by the quotobot team');
 const simpleEmbed = (text, attr) => {
     const toReturn = new Discord.MessageEmbed()
         .setColor(6765239)
@@ -86,6 +113,30 @@ client.on('message', message => {
             break;
         case 'Haters':
             message.channel.send(simpleEmbed('How that stupid, dull Englishman ever came to be admitted within the intellectual circle which revolved round “the cleverest woman in Europe,” as her friends unanimously called her, no one ventured to guess—a golden key is said to open every door, asserted the more malignantly inclined.', sp));
+            break;
+        case 'weather':
+            message.channel.send(simpleEmbed(        axios
+                .get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=${configFile["weather-token"]}`
+                )
+                .then(response => {
+                    let apiData = response;
+                    let currentTemp = Math.ceil(apiData.data.main.temp)
+                    let maxTemp = apiData.data.main.temp_max;
+                    let minTemp = apiData.data.main.temp_min;
+                    let humidity = apiData.data.main.humidity;
+                    let wind = apiData.data.wind.speed;
+                    let author = message.author.username
+                    let profile = message.author.displayAvatarURL
+                    let icon = apiData.data.weather[0].icon
+                    let cityName = args
+                    let country = apiData.data.sys.country
+                    let pressure = apiData.data.main.pressure;
+                    let cloudness = apiData.data.weather[0].description;
+                    message.channel.send(exampleEmbed(currentTemp, maxTemp, minTemp, pressure, humidity, wind, cloudness, icon, author, profile, cityName, country));
+                }).catch(err => {
+                    message.reply(`Enter a valid city name`)
+                })));
             break;
         default:
             break;
