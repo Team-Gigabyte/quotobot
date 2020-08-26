641146cacaeb0d6efb2caa12ef2909ddbe351225
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-08-26 12:58:09 -0400

    ~weather imperial New+York works

--- index.js
+++ index.js
@@ -124,23 +124,22 @@ client.on('message', message => {
             let units = args[0] == "metric" || args[0] == "imperial" ? args[0] : "metric";
             let city = !args[0] == "metric" || !args[0] == "imperial" ? args[0] : args[1];
             axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${args[1]}&units=${units}&APPID=${configFile["weather-token"]}`
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=${configFile["weather-token"]}`
             )
                 .then(response => {
                     let apiData = response;
-                    let currentTemp = Math.round(apiData.data.main.temp)
+                    let currentTemp = Math.round(apiData.data.main.temp);
                     let maxTemp = Math.round(apiData.data.main.temp_max);
                     let minTemp = Math.round(apiData.data.main.temp_min);
                     let humidity = apiData.data.main.humidity;
                     let wind = apiData.data.wind.speed;
-                    let author = message.author.username
-                    let profile = message.author.displayAvatarURL
-                    let icon = apiData.data.weather[0].icon
-                    let cityName = args[1]
-                    let country = apiData.data.sys.country
+                    let author = message.author.username;
+                    let profile = message.author.displayAvatarURL;
+                    let icon = apiData.data.weather[0].icon;
+                    let country = apiData.data.sys.country;
                     let pressure = apiData.data.main.pressure;
                     let cloudness = apiData.data.weather[0].description;
-                    message.reply(exampleEmbed(currentTemp, maxTemp, minTemp, pressure, humidity, wind, cloudness, icon, author, profile, cityName, country));
+                    message.reply(exampleEmbed(currentTemp, maxTemp, minTemp, pressure, humidity, wind, cloudness, icon, author, profile, city, country));
                 }).catch(err => {
                     console.log(err);
                     message.reply("There was an error getting the weather.")
