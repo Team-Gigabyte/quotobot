42e80a84a0860c4f52ffd7a1ff50caac0148a746
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-08-29 17:10:02 -0400

    All quotes are in the DB

--- jsonToDb.py
+++ jsonToDb.py
@@ -3,10 +3,12 @@ import sqlite3
 import json
 data = json.load(open("quotes.json"))
 db = sqlite3.connect("quotes2.db")
-count = 0
-for q in data["Thomas Jefferson"]:
+count = 67
+source = "Scarlet Pimpernel by Baroness Orczy"
+for q in data[source]:
     count += 1
-    db.cursor().execute("insert into Quotes(quote, id, source, usage) values('" +q+"}', "+str(count)+", 'Thomas Jefferson', 0)")
+    #db.cursor().execute("insert into Quotes(quote, id, source, usage) values('" +q+"}', "+str(count)+", 'George Washington', 0)")
+    db.cursor().execute("insert into Quotes(quote, id, source, usage) values(?,?,?,?)",(q, count, source, 0))
 #print(data["Thomas Jefferson"])
 db.commit()
 db.close()
