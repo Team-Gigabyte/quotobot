0d2c1cc0b68a4f5634cec962a38e65dc3965da2d
BGV <25331232+bhargavgv@users.noreply.github.com>
2020-09-21 18:31:32 -0400

    Update portraits.js

--- db/portraits.js
+++ db/portraits.js
@@ -1,14 +1,17 @@
+/*
+[1] https://www.loc.gov/rr/print/list/057_intr.html
+*/
 module.exports = {
-  "George Washington": "https://cdn.discordapp.com/attachments/746526474131210281/756892295693074542/Screen_Shot_2020-09-19_at_10.59.27_AM.png",
-  "John Adams": "https://cdn.discordapp.com/attachments/746526474131210281/756892492301074442/Screen_Shot_2020-09-19_at_11.00.24_AM.png",
-  "Thomas Jefferson": "https://cdn.discordapp.com/attachments/746526474131210281/756892685880524992/Screen_Shot_2020-09-19_at_11.01.13_AM.png",
-  "James Madison": "https://cdn.discordapp.com/attachments/746526474131210281/756892775559200839/Screen_Shot_2020-09-19_at_11.01.35_AM.png",
-  "Scarlet Pimpernel by Baroness Orczy": "https://cdn.discordapp.com/attachments/746526474131210281/756893205794127872/Screen_Shot_2020-09-19_at_11.03.14_AM.png",
-  "John F. Kennedy": "https://cdn.discordapp.com/attachments/746526474131210281/756893393581506640/Screen_Shot_2020-09-19_at_11.04.02_AM.png",
-  "Barack Obama": "https://cdn.discordapp.com/attachments/746526474131210281/756893541095178330/Screen_Shot_2020-09-19_at_11.04.38_AM.png",
+  "George Washington": "https://cdn.discordapp.com/attachments/746526474131210281/756892295693074542/Screen_Shot_2020-09-19_at_10.59.27_AM.png", // 1
+  "John Adams": "https://cdn.discordapp.com/attachments/746526474131210281/756892492301074442/Screen_Shot_2020-09-19_at_11.00.24_AM.png", // 1
+  "Thomas Jefferson": "https://cdn.discordapp.com/attachments/746526474131210281/756892685880524992/Screen_Shot_2020-09-19_at_11.01.13_AM.png", // 1
+  "James Madison": "https://cdn.discordapp.com/attachments/746526474131210281/756892775559200839/Screen_Shot_2020-09-19_at_11.01.35_AM.png", // 1
+  "Scarlet Pimpernel by Baroness Orczy": "https://cdn.discordapp.com/attachments/746526474131210281/756893205794127872/Screen_Shot_2020-09-19_at_11.03.14_AM.png", // https://en.wikipedia.org/wiki/File:The_Scarlet_Pimpernel_1982_film_dvd_cover.jpg fair use
+  "John F. Kennedy": "https://cdn.discordapp.com/attachments/746526474131210281/756893393581506640/Screen_Shot_2020-09-19_at_11.04.02_AM.png", // 1
+  "Barack Obama": "https://cdn.discordapp.com/attachments/746526474131210281/756893541095178330/Screen_Shot_2020-09-19_at_11.04.38_AM.png", // 1
   "Alexander Hamilton": "https://cdn.discordapp.com/attachments/746526474131210281/756893805298319390/Hamilton.jpg",
-  "Abraham Lincoln": "https://cdn.discordapp.com/attachments/746526474131210281/756894035473465414/Screen_Shot_2020-09-19_at_11.06.35_AM.png",
-  "Elon Musk": "https://cdn.discordapp.com/attachments/746526474131210281/756894178755084338/Screen_Shot_2020-09-19_at_11.07.10_AM.png", // from https://commons.wikimedia.org/wiki/File:Elon_Musk_Royal_Society.jpg CC BY-SA
+  "Abraham Lincoln": "https://cdn.discordapp.com/attachments/746526474131210281/756894035473465414/Screen_Shot_2020-09-19_at_11.06.35_AM.png", // 1
+  "Elon Musk": "https://cdn.discordapp.com/attachments/746526474131210281/756894178755084338/Screen_Shot_2020-09-19_at_11.07.10_AM.png", // https://commons.wikimedia.org/wiki/File:Elon_Musk_Royal_Society.jpg CC BY-SA
   "Eleanor Roosevelt": "https://cdn.discordapp.com/attachments/746526474131210281/756894467893624912/Eleanor_Roosevelt_portrait_1933.jpg",
   "QB Devs": "https://cdn.discordapp.com/attachments/449680513683292162/755533965657505843/unknown.png"
 }
