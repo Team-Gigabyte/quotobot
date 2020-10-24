cb8dcbceef2bf895387380d73e92eed51a5f9285
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-10-24 19:40:06 -0400

    Update update_wiki.sh

--- update_wiki.sh
+++ update_wiki.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 CURRWD=$(pwd)
-URL=$(git config --get remote.origin.url)
 mkdir -p ~/wikitemp
 cd ~/wikitemp
-git clone $URL.wiki .
-for i in *.md; do cat i > CURRWD/wiki/i; done
+git clone https://github.com/Team-Gigabyte/quotobot.wiki .
+for i in *.md; do cat i > $CURRWD/wiki/i; done
