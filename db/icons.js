d132d197b67ff08355f908786d62c86badad6e8b
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-10-30 15:14:17 -0400

    Update icons.js

--- db/icons.js
+++ db/icons.js
@@ -1,5 +1,5 @@
 module.exports = {
-    quote: "https://cdn.discordapp.com/attachments/",// from https://materialdesignicons.com/icon/comment-quote licensed under SIL OFL
+    quote: "https://cdn.discordapp.com/attachments/449680513683292162/771800588446335026/qblogo.png", // from https://materialdesignicons.com/icon/comment-quote licensed under SIL OFL
     empty: "https://cdn.discordapp.com/attachments/449680513683292162/746829996752109678/Untitled.png",
     info: "https://cdn.discordapp.com/attachments/449680513683292162/748682998022537287/information_2139.png", // this and below from Twemoji licensed under CC-BY 4.0
     warn: "https://cdn.discordapp.com/attachments/449680513683292162/751892501375221862/warning_26a0.png"
