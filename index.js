3e5067ed863b314eaebe0086e2b1ff11b0020bf0
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-10-30 15:07:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -74,9 +74,9 @@ const regex = Object.freeze({
     url: /^(http|https):\/\/[^ "]+$/
 })
 const icons = Object.freeze({
-    quote: "https://cdn.discordapp.com/attachments/449680513683292162/755533965657505843/unknown.png",// from https://materialdesignicons.com/icon/comment-quote licensed under SIL OFL
+    quote: "https://cdn.discordapp.com/attachments/",// from https://materialdesignicons.com/icon/comment-quote licensed under SIL OFL
     empty: "https://cdn.discordapp.com/attachments/449680513683292162/746829996752109678/Untitled.png",
-    info: "https://cdn.discordapp.com/attachments/449680513683292162/748682998022537287/information_2139.png", // this and below from Twemoji license under CC-BY 4.0
+    info: "https://cdn.discordapp.com/attachments/449680513683292162/748682998022537287/information_2139.png", // this and below from Twemoji licensed under CC-BY 4.0
     warn: "https://cdn.discordapp.com/attachments/449680513683292162/751892501375221862/warning_26a0.png"
 })
 const sp = "📕 Scarlet Pimpernel by Baroness Orczy";
