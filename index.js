f7f15fefc1bde7dd6ab409b7ec6be9bc628b2303
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-10-30 15:09:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -73,12 +73,7 @@ const regex = Object.freeze({
     escape: str => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
     url: /^(http|https):\/\/[^ "]+$/
 })
-const icons = Object.freeze({
-    quote: "https://cdn.discordapp.com/attachments/",// from https://materialdesignicons.com/icon/comment-quote licensed under SIL OFL
-    empty: "https://cdn.discordapp.com/attachments/449680513683292162/746829996752109678/Untitled.png",
-    info: "https://cdn.discordapp.com/attachments/449680513683292162/748682998022537287/information_2139.png", // this and below from Twemoji licensed under CC-BY 4.0
-    warn: "https://cdn.discordapp.com/attachments/449680513683292162/751892501375221862/warning_26a0.png"
-})
+const icons = require("./db/icons.js");
 const sp = "📕 Scarlet Pimpernel by Baroness Orczy";
 const randQuoteQuery = "SELECT quote, source FROM Quotes WHERE id IN (SELECT id FROM Quotes ORDER BY RANDOM() LIMIT 1);";
 const usedWeatherRecently = new Set(), usedStocksRecently = new Set(), usedLeagueRecently = new Set();
