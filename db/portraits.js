f55fd8d7ac885c0d0efa62e6bce1018435e92b05
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2022-11-23 22:34:08 -0500

    Update portraits.js

--- db/portraits.js
+++ db/portraits.js
@@ -1,5 +1,6 @@
 /*
 [1] https://www.loc.gov/rr/print/list/057_intr.html
+Note: imgur hosting is not for commercial use
 */
 module.exports = {
   "George Washington": "https://cdn.discordapp.com/attachments/746526474131210281/756892295693074542/Screen_Shot_2020-09-19_at_10.59.27_AM.png", // 1
@@ -8,7 +9,7 @@ module.exports = {
   "James Madison": "https://cdn.discordapp.com/attachments/746526474131210281/756892775559200839/Screen_Shot_2020-09-19_at_11.01.35_AM.png", // 1
   "Scarlet Pimpernel by Baroness Orczy": "https://cdn.discordapp.com/attachments/746526474131210281/756893205794127872/Screen_Shot_2020-09-19_at_11.03.14_AM.png", // https://en.wikipedia.org/wiki/File:The_Scarlet_Pimpernel_1982_film_dvd_cover.jpg fair use
   "John F. Kennedy": "https://cdn.discordapp.com/attachments/746526474131210281/756893393581506640/Screen_Shot_2020-09-19_at_11.04.02_AM.png", // 1
-  "Barack Obama": "https://cdn.discordapp.com/attachments/746526474131210281/756893541095178330/Screen_Shot_2020-09-19_at_11.04.38_AM.png", // 1
+  "Barack Obama": "https://i.imgur.com/I6IPA6V.jpg", // https://commons.wikimedia.org/wiki/File:Official_portrait_of_Barack_Obama.jpg public domain since from federal govt
   "Alexander Hamilton": "https://cdn.discordapp.com/attachments/746526474131210281/756893805298319390/Hamilton.jpg", // https://commons.wikimedia.org/wiki/File:Alexander_Hamilton_portrait_by_John_Trumbull_1806.jpg
   "Abraham Lincoln": "https://cdn.discordapp.com/attachments/746526474131210281/756894035473465414/Screen_Shot_2020-09-19_at_11.06.35_AM.png", // 1
   "Elon Musk": "https://cdn.discordapp.com/attachments/746526474131210281/756894178755084338/Screen_Shot_2020-09-19_at_11.07.10_AM.png", // https://commons.wikimedia.org/wiki/File:Elon_Musk_Royal_Society.jpg CC BY-SA
