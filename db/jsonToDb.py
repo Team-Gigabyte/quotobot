72f51d9e060a5f30036d3dd94cd281c9fc91f09e
Sourcery AI <>
2023-01-22 04:21:46 +0000

    'Refactored by Sourcery'

--- db/jsonToDb.py
+++ db/jsonToDb.py
@@ -3,10 +3,8 @@ import sqlite3
 import json
 data = json.load(open("quotes.json"))
 db = sqlite3.connect("quotes2.db")
-count = 67
 source = "Scarlet Pimpernel by Baroness Orczy"
-for q in data[source]:
-    count += 1
+for count, q in enumerate(data[source], start=68):
     #db.cursor().execute("insert into Quotes(quote, id, source, usage) values('" +q+"}', "+str(count)+", 'George Washington', 0)")
     db.cursor().execute("insert into Quotes(quote, id, source, usage) values(?,?,?,?)",(q, count, source, 0))
 #print(data["Thomas Jefferson"])
